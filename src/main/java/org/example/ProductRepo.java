89d9d5c2ae1b5039a8d92c50508abb2351b847aa
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-06-05 11:33:53 +0200

    ProductRepo change from Map to List

--- src/main/java/org/example/ProductRepo.java
+++ src/main/java/org/example/ProductRepo.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProductRepo {
@@ -12,25 +14,40 @@ Step 1: Create a record Product with the necessary attributes.
 Step 2: Implement the ProductRepo class with a list to store products.
 Step 3: Implement methods to add, remove, and get products (single product and all products).
      */
+List<Product> allProducts = new ArrayList<>();
 
-
-
-
-    Map<String,Product> allProducts= new HashMap<>();
-
-    public void add (Product product) {
-        allProducts.put(product.id(),product);
-    }
-    public void remove (Product product) {
-        allProducts.remove(product.id());
-    }
-    public String getOne (Product product) {
+public void add(Product product) {
+    allProducts.add(product);
+}
+public void remove(Product product) {
+    allProducts.remove(product);
+}
+public String getOne (Product product) {
         return product.toString();
-    }
-    public String getAll () {
+}
+public String getAll () {
         return allProducts.toString();
-    }
+}
 
+
+
+
+//
+//    Map<String,Product> allProducts= new HashMap<>();
+//
+//    public void add (Product product) {
+//        allProducts.put(product.id(),product);
+//    }
+//    public void remove (Product product) {
+//        allProducts.remove(product.id());
+//    }
+//    public String getOne (Product product) {
+//        return product.toString();
+//    }
+//    public String getAll () {
+//        return allProducts.toString();
+//    }
+//
 
 
 }
